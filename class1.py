67206f0c6c44f9fabdb6c23ad85ff7ba0f018de4
SyedaFiroza <forhadfiroza@gmail.com>
2022-10-30 22:28:17 +0600

    test

---
+++ class1.py
@@ -0,0 +1,24 @@
+
+
+
+
+print('Hello')
+# hel
+# beta
+# kete
+"""
+This is
+a 
+Comment
+line.
+"""
+name = 'nila'
+
+if name == 'nila':
+    print('ok')
+
+a = 4
+b = str(a)
+print(type(a))
+print(type(b))
+print(type(str(a)))
---
+++ class2.py
@@ -0,0 +1,66 @@
+a = 5
+A = 4.5
+c = a + A
+print(c)
+print(type(c))
+
+x = '5'
+print(type(x))
+
+y = int(x)
+print(type(y))
+
+z = float(y)
+print(type(z))
+
+m = bool(z)
+print(type(m))
+
+# Operator
+
+a = 5 + 3
+print(a)
+
+b = 5 - 3
+print(b)
+
+c = 5 * 3
+print(c)
+
+d = 5 % 3
+print(d)
+
+e = 5 ** 3
+print(e)
+
+f = 5 // 3
+print(f)
+
+q = 5
+r = 3
+
+# q+=r
+# print(q)
+
+# q-=r
+# print(q)
+
+# q*=r
+# print(q)
+
+# q/=r
+# print(q)
+
+# q%=r
+# print(q)
+
+# q//=r
+# print(q)
+
+# q**=r
+# print(q)
+
+q&=r
+print(q)
+
+
